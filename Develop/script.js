b39317bc7d71b1d56cf2de353a805ed777226125
merikettapearl212 <68967132+merikettapearl212@users.noreply.github.com>
2020-10-31 15:41:53 -0700

    updates

--- Develop/script.js
+++ Develop/script.js
@@ -4,10 +4,10 @@ var generateBtn = document.querySelector("#generate");
 
 //Define database of characters for password
 var passwordCharacter = {
-  lowerCase: ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"],
-  upperCase: ["A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z"],
-  specialCharacters: ["!,@,#,$,%,^,&,*,,~,`,+,=,/,\,{,,},[,],<,>,?,,,:,;,."],
-  numbers: ["0,1,2,3,4,5,6,7,8,9"],
+  lowerCase: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
+  upperCase: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
+  specialCharacters: ["!", "@", "#", "$", "%", "^", "&", "*", "~", "+", "=", "/", "{", "}", "[", "]", "<", ">", "?", ":", ";", "."],
+  numbers: ["0, 1, 2, 3, 4, 5, 6, 7, 8, 9"],
 }
 
 //Click function button
@@ -17,10 +17,10 @@ function generatePassword() {
   var characterLot = [];
 
   //Ask prompt user for password length. Use parsInt so string is made into a number nalue
-  var totalLength = parsenInt (prompt ("How long would you like your password to be? (Must be between 8-128 characters)?"));
+  var totalLength = parseInt (prompt ("How long would you like your password to be? (Must be between 8-128 characters)?"));
   
   //If not valid length, prompt again for length
-    if (!(totalLength >= 8 || totalLength <= 128)) {
+    if (totalLength <= 8 || totalLength >= 128) {
       alert ("Must be between 8-128 characters!");
       return;
     }
@@ -37,23 +37,22 @@ function generatePassword() {
       }
     
     //Ask confirm use of special characters.
-    var useSpecial= confirm ("Would you like to include special characters?");
+    var useSpecial = confirm ("Would you like to include special characters?");
       if (useSpecial === true) {
         characterLot = characterLot.concat(passwordCharacter.specialCharacters);
       }
 
     //Ask and confirm use of numbers.
-    var useNumber= confirm = ("Would you like to include numbers?");
+    var useNumber = confirm = ("Would you like to include numbers?");
       if (useNumber === true) {
         characterLot = characterLot.concat(passwordCharacter.numbers);
       }
 
-      if(useLowercase === false && useUppercase === false && useSpecial === false && useNumber === false) {
+      if (useLowercase === false && useUppercase === false && useSpecial === false && useNumber === false) {
         alert("You must select at least one character type.");
         return;
       }
 
-      alert ("You have selected: Length-" + totalLength + "; Lowercase-" + useLowercase + "; Uppercase-" + useUppercase + "; Special Characters" + useSpecial + "; Numbers" + useNumber);
 
       var passwordOut = [];
 
