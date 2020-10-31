b0555101539d3acebaad9ab4703d9addda2e0cd1
merikettapearl212 <68967132+merikettapearl212@users.noreply.github.com>
2020-10-31 14:43:10 -0700

    updating js 1st prompt

--- Develop/script.js
+++ Develop/script.js
@@ -2,19 +2,7 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-
-//requirement arrays
+//Define database of characters for password
 var passwordCharacter = {
   lowerCase: ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"],
   upperCase: ["A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z"],
@@ -22,22 +10,24 @@ var passwordCharacter = {
   numbers: ["0,1,2,3,4,5,6,7,8,9"],
 }
 
-
-//prompt ask user for password criteria
-//user's password between 8-128 characters
+//Click function button
 function generatePassword() {
   
-  //prompt ask user for password length.
-  var totalLength= prompt ("How long would you like your password to be? (Must be between 8-128 characters)?");
+  //Create variable that serves as whole lot for loop.
+  var characterLot = [];
+
+  //Ask prompt user for password length. Use parsInt so string is made into a number nalue
+  var totalLength = parsenInt (prompt ("How long would you like your password to be? (Must be between 8-128 characters)?"));
   
-    if (totalLength >= 8 || totalLength <= 128) {
+  //If not valid length, prompt again for length
+    if (!(totalLength >= 8 || totalLength <= 128)) {
       alert ("Must be between 8-128 characters!");
       return;
     }
-  }
+}
   //confirm use of lowercase letters
   //var useLowercase = confirm ("Would you like to include lowercase letters?");
-   // if (useLowercase === true) { }
+    //if (useLowercase === true) { }
   //confirm use of uppercase letters.
   //var useUppercase = confirm ("Would you like to include uppercase letters?");
     //if (useUppercase === true) {}
@@ -57,6 +47,14 @@ function generatePassword() {
 //   [Math.floor(Math.random() * passwordCharacter.length)];
 //  console.log();
 //}
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
 
+  passwordText.value = password;
+}
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
 
